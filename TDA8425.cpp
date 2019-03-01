f767a5a413f1b136cfb883fb4a9fe260ea5f1fe2
Andy Karpov <andy.karpov@gmail.com>
2019-03-01 17:05:43 +0200

    refactored input values

--- TDA8425.cpp
+++ TDA8425.cpp
@@ -13,51 +13,56 @@
                                  /* values for those registers: */   
 #define TDA8425_S1_OFF     0xEE  /* audio off (mute on) */   
 #define TDA8425_S1_ON      0xCE  /* audio on (mute off) - "linear stereo" mode */   
-   
-void TDA8425::Write(const int Subaddr, const int Value) {
-	Wire.beginTransmission(TDA8425_ADDR);
-	Wire.write(Subaddr);
-	Wire.write(Value);
-	Wire.endTransmission();
-}
 
-void TDA8425::Restart()   
-{   
-    Write(TDA8425_VL, 0x3C |0xC0);   
-    Write(TDA8425_VR, 0x3C |0xC0);   
-    Write(TDA8425_BA, 0x6  |0xF0);   
-    Write(TDA8425_TR, 0x6  |0xF0);   
-    Write(TDA8425_S1, TDA8425_S1_OFF); /* mute */   
-}   
-   
-TDA8425::TDA8425() {
-	Wire.begin();
-    TDA8425::Restart();   
-}   
-   
-void TDA8425::SetMute(int Mute)
+TDA8425::TDA8425() 
 {
-    Write(TDA8425_S1, (Mute) ? TDA8425_S1_ON : TDA8425_S1_OFF);  
+}
+   
+void TDA8425::begin()   
+{   
+    Wire.begin();
+    write(TDA8425_VL, 0x3C |0xC0);   
+    write(TDA8425_VR, 0x3C |0xC0);   
+    write(TDA8425_BA, 0x6  |0xF0);   
+    write(TDA8425_TR, 0x6  |0xF0);   
+    write(TDA8425_S1, TDA8425_S1_OFF); /* mute */   
+}
+   
+void TDA8425::write(byte subaddr, byte value) 
+{
+    Wire.beginTransmission(TDA8425_ADDR);
+    Wire.write(subaddr);
+    Wire.write(value);
+    Wire.endTransmission();
+}
+   
+   
+void TDA8425::setMute(bool mute)
+{
+    write(TDA8425_S1, (mute) ? TDA8425_S1_ON : TDA8425_S1_OFF);  
 }   
    
-void TDA8425::SetVolume(const int Left,const int Right) /* 0x3C = 0dB */   
+void TDA8425::setVolume(const int left, const int right) /* 0x3C = 0dB */   
 {   
-    int AdjLeft  = (Left /546) + 0x3C;   
-    int AdjRight = (Right/546) + 0x3C;   
-    AdjLeft = (AdjLeft > 0x3F) ? 0x3F: AdjLeft;   
-    AdjRight = (AdjRight > 0x3F) ? 0x3F: AdjRight;   
-   
-    Write(TDA8425_VL, AdjLeft  | 0xC0);   
-    Write(TDA8425_VR, AdjRight | 0xC0);   
+    byte l = map(left, 0, 100, 28, 63);
+    byte r = map(right, 0, 100, 28, 63);
+
+    l = constrain(l, 28, 63);
+    r = constrain(l, 28, 63);
+
+    write(TDA8425_VL, l  | 0xC0);   
+    write(TDA8425_VR, r | 0xC0);   
 }    
    
-void TDA8425::SetBassTreble(const int Bass,const int Treble)    
-{   
-    int AdjBass   = (Bass   / 3641) + 0x6;   
-    int AdjTreble = (Treble / 3641) + 0x6;   
-    AdjBass   = (AdjBass   < 0) ? 0: AdjBass;   
-    AdjTreble = (AdjTreble < 0) ? 0: AdjTreble;   
-   
-    Write(TDA8425_BA, AdjBass   |0xF0);    
-    Write(TDA8425_TR, AdjTreble |0xF0);    
+void TDA8425::setBassTreble(const int bass, const int treble)    
+{
+    byte b = map(bass, 0, 100, 0, 15);
+    byte t = map(treble, 0, 100, 0, 15);
+
+    b = constrain(b, 0, 15);
+    t = constrain(t, 0, 15);
+
+    write(TDA8425_BA, b | 0xF0);    
+    write(TDA8425_TR, t | 0xF0); 
 }
+
--- TDA8425.h
+++ TDA8425.h
@@ -1,21 +1,5 @@
 /*
  * TDA8425.h
- * defintions for TDA8425 audio processor.
- * Created by Andrey Karpov
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #ifndef _TDA8425_H_
@@ -23,21 +7,17 @@
 
 #include <Arduino.h>
 
-class TDA8425 { 
- 
-	public: 
-		TDA8425(); 
-		virtual void Write(const int Subaddr, const int Value);
-		virtual void SetMute(const int Mute) ; 
-		virtual void SetVolume(const int Left,const int Right) /* 0 = Gain 1 */ ; 
-		virtual void SetBassTreble(const int Bass,const int Treble)  ; 
- 
-		virtual bool IsVolumeAvailable()	{ return true; } 
-		virtual bool IsBassTrebleAvailable(){ return true; } 
- 
-		// To support Hibernate mode 
-		virtual void Restart( void ); 
- 
-}; 
- 
+class TDA8425 {
+
+	private:
+		virtual void write(const byte subaddr, const byte value);
+
+	public:
+		TDA8425();
+		virtual void begin(void);
+		virtual void setMute(const bool mute);
+		virtual void setVolume(const int left, const int right);
+		virtual void setBassTreble(const int bass,const int treble);
+};
+
 #endif  // _TDA8425_H_
